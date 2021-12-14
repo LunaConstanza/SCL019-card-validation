ba3483535b702a08b31df593dcb74b7cd47d6596
Luna Gonzalez <lunagonb@gmail.com>
2021-12-14 09:44:06 -0300

    mi primer guardado en github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import validator from './validator.js';
 
-console.log(validator);
+const formDon = document.getElementById('donation');
+let card = document.getElementById('number');
+
+formDon.addEventListener('submit', function(e){
+    e.preventDefault();
+    let finalAnswer;
+    let finalValidation = validator.isValid(card.value)
+    //console.log(finalValidation);
+    if (finalValidation==true){
+        finalAnswer = 'valida.'
+    } else {
+        finalAnswer = 'invalida. Vuelva a intentarlo'
+    }
+    let hiddeNum = validator.maskify(card.value);
+    document.getElementById("answerText").innerHTML = 'Su tarjeta ' + hiddeNum + ' es ' + finalAnswer;
+});
+//LINKS
+
+//POPUP
+let btnOpenPopup = document.getElementById('btnPopup');
+let overlay = document.getElementById('overlay');
+let popup = document.getElementById('popup');
+let namesUser = document.getElementById('names');
+
+btnOpenPopup.addEventListener('click', function(e){
+    e.preventDefault();
+    overlay.classList.add('active');
+    popup.classList.add('active');
+                //NAME
+    let separateNames = namesUser.value.split(' ');
+    let firstName = separateNames[0];
+    //console.log(firstName);
+    document.getElementById("nameUser").innerHTML = firstName;
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
+function transNumber (numberV){
+    if (numberV>9){
+      let separateNumberV = numberV.toString().split('');
+        //console.log(separateNumberV);
+      let newNumberV = separateNumberV.reduce(function (a, b){
+        let num1 = parseInt(a);
+        let num2 = parseInt(b);
+        return num1+num2;
+      });
+      //console.log(newNumberV);
+      return newNumberV;
+    } else {
+        return numberV;
+    }
+}
 const validator = {
-  // ...
+  isValid: function (creditCardNumber){
+    let arrayNumberCard = creditCardNumber.split('').reverse();
+        //console.log(arrayNumberCard);
+    let newArrayNumber = arrayNumberCard.map(function (v, index){
+        let newElement;
+        if (index % 2){                                                   //consultar
+            newElement = transNumber(v*2);
+        }
+        else {
+            newElement = parseInt(v);
+        }
+        return newElement;
+    });
+        // console.log(newArrayNumber);
+  let addNumbersCard = newArrayNumber.reduce(function(a, b){
+        return a+b;
+    });
+        //console.log(addNumbersCard);
+        if (addNumbersCard % 10 == 0){
+            //console.log('true');
+            return true;
+        } else {
+            //console.log('false');
+            return false;
+    }
+  }, 
+  maskify: function (creditCardNumber){
+    let hiddeNumber = [];
+      for(let ind = 0; ind < creditCardNumber.length; ind++){
+        if(ind < creditCardNumber.length-4){
+          hiddeNumber.push('#');
+        }else{
+          hiddeNumber.push(creditCardNumber[ind]);
+        }
+      }
+      return hiddeNumber.join('');
+  },
 };
-
 export default validator;
